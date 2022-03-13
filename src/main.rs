3b8a1c9860e24f57927079dddd471a4b58254e85
benharmonics <benjamin.jiron+git@gmail.com>
2022-03-13 03:59:51 -0600

    Organized use statements in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
+use caesar_cipher::Config;
+use std::{env, process};
 
 fn main() {
-    let config = caesar_cipher::Config::new(std::env::args())
+    let config = Config::new(env::args())
         .unwrap_or_else(|err| {
             eprintln!("{}", err);
-            std::process::exit(1);
+            process::exit(1);
         });
     
     if let Err(e) = caesar_cipher::run(config) {
         eprintln!("Application error: {}", e);
-        std::process::exit(1);
+        process::exit(1);
     }
 }
